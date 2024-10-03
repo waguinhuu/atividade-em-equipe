02d3d6680c28ef5a38f186f9709f443614069f86
Wagner Silva <156625511+waguinhuu@users.noreply.github.com>
2024-10-03 14:18:30 +0000

    Arquivo 2, resolução de conflito

--- arquivo_2.py
+++ arquivo_2.py
@@ -2,3 +2,20 @@ print("Adriano")
 
 
 print("Fim do projeto.")
+
+# Resoluçcão de conflito
+# git rebase
+
+# Apague as linhas indesejadas
+
+# git add .
+
+# git commit -m "Mensagem"
+
+# git rebase --continue
+
+#git pull
+
+# git pull
+
+#git push
