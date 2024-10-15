3a5e652aedb0b0828579f68fde911e7e8e67074f
Wagner Silva <156625511+waguinhuu@users.noreply.github.com>
2024-10-15 11:07:05 +0000

    commit da classe Endereço

--- arquivo_1.py
+++
@@ -1,7 +0,0 @@
-print("Wagner Silva")
-
-# git pull
-# git add .
-# git commit -m "Mensagem"
-# git pull
-# git push
---
+++ project/models/endereco.py
@@ -0,0 +1,8 @@
+class Endereco:
+    def __init__(self, logradouro: str, numero: str, complemento: str, cep: str, cidade: str):
+        self.logradouro = logradouro
+        self.numero = numero
+        self.complemento = complemento
+        self.cep = cep
+        self.cidade = cidade
+        
